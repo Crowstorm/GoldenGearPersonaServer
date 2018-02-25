f7615ff41619fab19a81c708c245ed3aff20487d
Big Paws <crowstormofficial@gmail.com>
2018-02-25 12:52:03 +0100

    changed2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.send({hi: 'elo'});
+    res.send({hi: 'eloszka'});
 });
 
 const PORT = process.env.PORT || 8000;
