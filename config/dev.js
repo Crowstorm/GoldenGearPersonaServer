1850e43260e103cadc145e7051d24dc5aa9e2ddd
Big Paws <crowstormofficial@gmail.com>
2018-03-06 23:41:19 +0100

    d

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    googleClientID: '992821279611-danslgb1q4p5qhgf2uu4koqnhbum1oak.apps.googleusercontent.com',
+    googleClientSecret: 'wFRWjyEO8RhaoezZezbaU7EE',
+    mongoURI: 'mongodb://crowstorm:Pokemon123@ds235328.mlab.com:35328/goldengearpersona-dev',
+    cookieKey: 'hru2hr279r782ghrf2y28gty2gthwgfbhiowet78ety'
+}
