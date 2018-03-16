1a2e206fa1784b27522a8a003219072998a93014
Big Paws <crowstormofficial@gmail.com>
2018-03-16 07:09:29 +0100

    stats

--- routes/characterRoutes.js
+++ routes/characterRoutes.js
@@ -2,19 +2,26 @@ const mongoose = require('mongoose');
 
 const User = require('../models/user');
 
-module.exports = app =>{
-    app.post('/api/createCharacter', (req, res) =>{
-        // if(!req.user){
-        //     return res.status(401).send({error: 'You must log in'})
-        // };
-        //const {id, name, title, classGame, portrait} = req.body;
+module.exports = app => {
+    app.post('/api/createCharacter', (req, res) => {
 
-        console.log('body',req.body);
-        //res.send(req.body)
-        
-        User.findOneAndUpdate({"googleId": req.body.id}, { $set: { "character.name": req.body.name, "character.title": req.body.title, "character.portrait": req.body.portrait, "character.classGame": req.body.classGame } }).then((result, err) =>{
+        let stats = {};
+        if (req.body.classGame === "warrior") {
+            stats = {
+                hp: 30,
+                mp: 10,
+                strength: 8,
+                defence: 7,
+                magic: 3,
+                magicResist: 3,
+                agility: 5,
+                luck: 5
+            }
+        }
+        console.log('statsy', stats)
+        User.findOneAndUpdate({ "googleId": req.body.id }, { $set: { "character.name": req.body.name, "character.title": req.body.title, "character.portrait": req.body.portrait, "character.classGame": req.body.classGame, "character.statistics": stats } }).then((result, err) => {
             console.log(result);
-            if(!err){
+            if (!err) {
                 res.status(200).send({
                     success: true,
                     'data': result
@@ -22,7 +29,7 @@ module.exports = app =>{
             } else {
                 res.send(err);
             }
-            
+
         })
     })
 }
